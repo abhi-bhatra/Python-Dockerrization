78c5a93005f94b11a9005f71575c3d740fd5073e
Abhinav Sharma <63901956+abhi-bhatra@users.noreply.github.com>
2021-11-26 17:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/about')
 def about():
-    return '<h1>This is a sample about</h1>'
+    return '<h1>This is a just about</h1>'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
